1d5cec1fc9efc14e844bd51db7ad824e9fc911cc
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2026-01-31 17:10:04 +0700

    add: adding model

--- openshopapp/models.py
+++ openshopapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=50)
+    shop = models.CharField(max_length=50)
+    price = models.IntegerField()
+    sku = models.CharField(max_length=50)
+    description = models.TextField()
+    location = models.CharField(max_length=50)
+    discount = models.IntegerField()
+    category = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    is_available = models.BooleanField()
+    picture = models.CharField(max_length=500)
